11bc61bfa70a53d7069219e17711d935db1ad6f4
Aisa03 <alessioaisa03@gmail.com>
2022-01-18 09:47:37 +0100

    situazione 2

--- app/src/main/java/com/example/voltify/MainActivity.java
+++ app/src/main/java/com/example/voltify/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnInserisci;
     EditText txtitolo;
     GestoreBrani gb;
+    Spinner spnGenere;
+    String[] elencoGeneri = {"Rock", "Liscio", "POP"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnInserisci=findViewById(R.id.btnInserisci);
         txtitolo=findViewById(R.id.plain);
+        spnGenere = (Spinner) findViewById(R.id.spnGenere);
+        ArrayAdapter<String> spGen = new ArrayAdapter<String>(this, R.layout.activity_main, elencoGeneri);
+        spnGenere.setAdapter(spGen);
+
         gb= new GestoreBrani();
 
         btnInserisci.setOnClickListener(
                 new Button.OnClickListener(){
                     public void onClick(View v){
+                        String testo = spnGenere.getSelectedItem().toString();
+                        Toast.makeText(getApplicationContext(), testo, Toast.LENGHT_LONG).show();
                         gb.addBrano(txtitolo.getText().toString());
                     }
                 }

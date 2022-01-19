95088d8c011c01f4d3b82ce22539b47ca24df1a6
alyno18 <alybonomi18@gmail.com>
2022-01-19 19:15:51 +0100

    situazione 3

--- app/src/main/java/com/example/voltify/GestoreBrani.java
+++ app/src/main/java/com/example/voltify/GestoreBrani.java
@@ -1,9 +1,23 @@
 package com.example.voltify;
 
+import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
 
-public class GestoreBrani {
+public class GestoreBrani extends AppCompatActivity {
+    ListView listaBrani;
     ArrayList<Brano> ListaBrani;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gestore_brani);
+        listaBrani=findViewById(R.id.listaBrani);
+    }
 
     public GestoreBrani(){
         ListaBrani = new  ArrayList<Brano>();
@@ -12,5 +26,9 @@ public class GestoreBrani {
     public void addBrano(String titolo){
         Brano br = new Brano(titolo);
         ListaBrani.add(br);
+    }
+
+    public void visualizza(){
+        listaBrani.setAdapter((ListAdapter) ListaBrani);
     }
 }
--- app/src/main/java/com/example/voltify/MainActivity.java
+++ app/src/main/java/com/example/voltify/MainActivity.java
@@ -1,26 +1,31 @@
 package com.example.voltify;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnInserisci;
+    Button btnVisualizza;
     EditText txtitolo;
     GestoreBrani gb;
     Spinner spnGenere;
-    String[] elencoGeneri = {"Rock", "Liscio", "POP"};
+    String[] elencoGeneri = {"Rock", "Rap", "Pop"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnInserisci=findViewById(R.id.btnInserisci);
-        txtitolo=findViewById(R.id.plain);
+        btnVisualizza=findViewById(R.id.btnVisualizza);
+        txtitolo=findViewById(R.id.titolo);
         spnGenere = (Spinner) findViewById(R.id.spnGenere);
         ArrayAdapter<String> spGen = new ArrayAdapter<String>(this, R.layout.activity_main, elencoGeneri);
         spnGenere.setAdapter(spGen);
@@ -30,9 +35,17 @@ public class MainActivity extends AppCompatActivity {
         btnInserisci.setOnClickListener(
                 new Button.OnClickListener(){
                     public void onClick(View v){
-                        String testo = spnGenere.getSelectedItem().toString();
-                        Toast.makeText(getApplicationContext(), testo, Toast.LENGHT_LONG).show();
                         gb.addBrano(txtitolo.getText().toString());
+                    }
+                }
+        );
+
+        btnVisualizza.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        gb.visualizza();
+                        Intent i = new Intent(MainActivity.this, GestoreBrani.class);
+                        startActivity(i);
                     }
                 }
         );
